8a7732ccc1a96b0d9ba5deff6169eb6e4663d151
Nick Yahnke <nthitz@gmail.com>
2015-04-25 18:55:15 -0700

    initial zoom

---
+++ zoom.js
@@ -0,0 +1,109 @@
+var img = new Image()
+img.onload = imageLoaded;
+img.src="computerPhoto3.jpg"
+var canvas = document.getElementById('canvas');
+var ctxt = canvas.getContext('2d');
+
+window.onmousemove = mousemove;
+window.onresize = resize;
+
+function imageLoaded() {
+  // document.body.appendChild(img);
+  resize();
+  d3.timer(animate);
+
+}
+var photoW = 1440;
+var photoH = 900;
+var aspectRatio = photoW / photoH;
+console.log(aspectRatio);
+var width = window.innerWidth;
+var height = window.innerHeight;
+var easing = d3.ease('cubic-in-out')
+var frames = 50;
+var globalTicker = 0;
+
+var zoomedInWidth = 529;
+var zoomedInHeight = 350
+// var zoomedInHeight = 1 / aspectRatio* zoomedInWidth;
+
+var translateX = 417;
+var translateY = 155;
+
+var rotationAmountDegrees = 6.4;
+var rotationAmount = Math.PI * rotationAmountDegrees / 180;
+
+var zoomInAmountX = width / zoomedInWidth;
+var zoomInAmountY = height / zoomedInHeight;
+
+var translateXAmount = zoomInAmountX * translateX;
+var translateYAmount = zoomInAmountY * translateY;
+
+var translateXScale = d3.scale.linear().range([0,-translateXAmount]);
+var translateYScale = d3.scale.linear().range([0,-translateYAmount]);
+
+var scaleXScale = d3.scale.linear().range([1,zoomInAmountX]);
+var scaleYScale = d3.scale.linear().range([1,zoomInAmountY]);
+
+var rotationScale = d3.scale.linear().range([0, rotationAmount]);
+
+var mouse = [0,0]
+var fromMouse = false;
+
+
+function mousemove(e) {
+  mouse[0] = e.clientX;
+  mouse[1] = e.clientY;
+}
+function resize() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  var curAspect = width / height;
+  if(curAspect > aspectRatio) {
+    console.log('cur bigger')
+    //fit to height
+    console.log(height)
+    width = aspectRatio * height;
+  } else if(curAspect < aspectRatio) {
+    console.log('cur smaller')
+    //fit to width
+    height = 1 / aspectRatio  * width;
+  }
+  canvas.width = width;
+  canvas.height = height;
+  console.log(width / height)
+}
+function animate() {
+  ctxt.clearRect(0,0, width, height);
+  ctxt.globalCompositeOperation = "multiply"
+  var globalPct = globalTicker / frames;
+  if(fromMouse) {
+    globalPct = mouse[0] / (window.outerWidth - 100);
+    while(globalPct > 1) { globalPct -= 1}
+  }
+  globalPct = easing(globalPct)
+  var ticker = globalPct * frames;
+  var iterations = 8
+  for(var i = 0; i < iterations; i++) {
+    ctxt.save();
+    var pct = (ticker + ((i - ~~(iterations / 2)) * 1.5)) / frames;
+    while(pct > 1) { pct -= 1}
+    while(pct < 0) { pct += 1}
+    ctxt.globalAlpha = 0.1
+    ctxt.translate(
+      translateXScale(pct), translateYScale(pct)
+    )
+    ctxt.scale(scaleXScale(pct), scaleYScale(pct))
+    ctxt.translate(width/2, height/2)
+    ctxt.rotate(rotationScale(pct))
+    ctxt.drawImage(img, -width/2,-height/2, width, height);
+
+
+    ctxt.restore();
+  }
+
+  globalTicker += 1;
+  if(globalTicker > frames) {
+    globalTicker = 0;
+  }
+}
